93ea76a0377bc4493ca6a9663df7d313f0afdd9c
sofia <selizondo003@insite.4cd.edu>
2022-05-19 11:34:01 -0700

    using const with fetch and .then

--- j/main.js
+++ j/main.js
@@ -1,52 +1,21 @@
-const quote = () => {
+
+const inspirationalQuote = ()  => {
     fetch("https://api.goprogram.ai/inspiration")
         .then(response => {
-            return response.json()
+            return response.json();
         })
-        .then( data => {
-            let a = 0;
-
-            let next = document.getElementById("next");
-            next.addEventListener('click', (e) => {
-                let countN = document.getElementById("countN");
-                countN.value = ++a;
-                inspoQuote(countN.value,data);
-            },false);
-        })
-        .catch((err) => console.log("Oops!", err));
-    
-
-        async function getQuote(){
-            let url = "https://api.goprogram.ai/inspiration";
-            try{
-                let response = await fetch(url);
-                return await response.json();
-            } catch(err) { console.log("Oops!", err);
-                }
-            }
-   
-        async function displayQuote(){
-            let quote = await getQuote();
-            let html ='';
-  
-  
-        }
-  
-        function inspoQuote(index, data){
-            let quoteAuother = document.getElementById("quoteAuthor");
-  
-            if(data[index].author == null) {
-                data[index].author ="unknown";
-            }
-            let displayQuote = `<div class="quoteDisplay">
-            <div>${data[index].quote}</div>
-            <div>${data[index].author}</div>
+        .then(response => {
+            let quote = document.getElementById("quote");
+            
+            let htmlQuote = `<div class="quoteDisplay">
+            <div>${quote}</div>
+            <div>${author}</div>
             </div>`;
-            quoteAuthor.innerHTML = displayQuote;
-        }
-            html += htmlSegment;
-        };
-  
-        let container = document.querySelector('.quote');
-        container.innerHTML = html;
- 
+            quote.innerHTML = htmlQuote;
+        })
+        .catch(err => {
+            console.log("Error", err);
+        })
+    }
+
+
