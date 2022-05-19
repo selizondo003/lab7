8a415d677f2f1dd727c93dd515ce1578ab1c0fab
sofia <selizondo003@insite.4cd.edu>
2022-05-19 10:54:02 -0700

    adding in get quote function using async

--- j/main.js
+++ j/main.js
@@ -1,7 +1,52 @@
+const quote = () => {
+    fetch("https://api.goprogram.ai/inspiration")
+        .then(response => {
+            return response.json()
+        })
+        .then( data => {
+            let a = 0;
 
-fetch("https://api.goprogram.ai/inspiration")
-.then(response =>response.json())
-.then(json => console.log(json))
-.catch(err =>{
-    console.error('oops', err.message);
-})
+            let next = document.getElementById("next");
+            next.addEventListener('click', (e) => {
+                let countN = document.getElementById("countN");
+                countN.value = ++a;
+                inspoQuote(countN.value,data);
+            },false);
+        })
+        .catch((err) => console.log("Oops!", err));
+    
+
+        async function getQuote(){
+            let url = "https://api.goprogram.ai/inspiration";
+            try{
+                let response = await fetch(url);
+                return await response.json();
+            } catch(err) { console.log("Oops!", err);
+                }
+            }
+   
+        async function displayQuote(){
+            let quote = await getQuote();
+            let html ='';
+  
+  
+        }
+  
+        function inspoQuote(index, data){
+            let quoteAuother = document.getElementById("quoteAuthor");
+  
+            if(data[index].author == null) {
+                data[index].author ="unknown";
+            }
+            let displayQuote = `<div class="quoteDisplay">
+            <div>${data[index].quote}</div>
+            <div>${data[index].author}</div>
+            </div>`;
+            quoteAuthor.innerHTML = displayQuote;
+        }
+            html += htmlSegment;
+        };
+  
+        let container = document.querySelector('.quote');
+        container.innerHTML = html;
+ 
