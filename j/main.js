b41f6ea6d37aaf82b60c63042f90af8acaf294d4
sofia <selizondo003@insite.4cd.edu>
2022-05-19 10:57:08 -0700

    display quote and next button

--- j/main.js
+++ j/main.js
@@ -1,7 +1,30 @@
+const quote = () => {
+    fetch("https://api.goprogram.ai/inspiration")
+        .then(response => {
+            return response.json()
+        })
+        .then( data => {
+            let a = 0;
 
-fetch("https://api.goprogram.ai/inspiration")
-.then(response =>response.json())
-.then(json => console.log(json))
-.catch(err =>{
-    console.error('oops', err.message);
-})
+            let next = document.getElementById("next");
+            next.addEventListener('click', (e) => {
+                let countN = document.getElementById("countN");
+                countN.value = ++a;
+                inspoQuote(countN.value,data);
+            },false);
+        })
+        .catch((err) => console.log("Oops!", err));
+    
+
+        function inspoQuote(index, data){
+            
+
+            if(data[index].author == null) {
+                data[index].author ="unknown";
+            }
+            let displayQuote = `<div class="quoteDisplay">
+            <div>${data[index].quote}</div>
+            <div>${data[index].author}</div>
+            </div>`;
+            quoteAuthor.innerHTML = displayQuote;
+        }}
