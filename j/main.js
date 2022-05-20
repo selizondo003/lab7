980edc703396229c701e86f8be85a89bb33186d1
sofia <selizondo003@insite.4cd.edu>
2022-05-19 18:35:56 -0700

    img api functioning

--- j/main.js
+++ j/main.js
@@ -1,21 +1,32 @@
 
 const inspirationalQuote = ()  => {
-    fetch("https://api.goprogram.ai/inspiration")
+    fetch("https://api.quotable.io/random")
         .then(response => {
             return response.json();
         })
         .then(response => {
-            let quote = document.getElementById("quote");
-            
-            let htmlQuote = `<div class="quoteDisplay">
-            <div>${quote}</div>
-            <div>${author}</div>
-            </div>`;
-            quote.innerHTML = htmlQuote;
+            document.getElementById("quote").innerHTML = response.content; 
+            document.getElementById("author").innerHTML = "- " + response.author;
         })
-        .catch(err => {
+      .catch(function(err) {
             console.log("Error", err);
-        })
+        });
     }
+    inspirationalQuote();
 
 
+
+const randomImg =() =>{
+    fetch("https://random.dog/woof.json")
+        .then(data => {
+            return data.json();
+        })
+        .then(data => {
+            const imgElement = document.getElementById("imgElement")
+            imgElement.src=data.url;
+        })
+        .catch(function(err) {
+            console.log("Error", err);
+        });
+};
+randomImg();
